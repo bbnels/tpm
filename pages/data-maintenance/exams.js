088496964f7b2eaa6171c22f3cb62e35d624ac05
bbnels <bianji.rose@myiuc.com>
2024-08-09 22:05:59 +0100

    Fix ReferenceError for deleteExams function

--- pages/data-maintenance/exams.js
+++ pages/data-maintenance/exams.js
@@ -40,7 +40,7 @@ export default function Exams() {
       <Table 
         headers={['name']}
         data={exams}
-        onDelete={deleteExams}
+        onDelete={deleteExam}  // corrected from `deleteExams` to `deleteExam`
       />
     </Layout>
   );
